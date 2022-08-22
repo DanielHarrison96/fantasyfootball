d843dbe1fc104924baa97f51cbca1124b347a7e0
jh2048 <accounts@jjkh.net>
2022-08-22 22:47:23 +0100

    Add player score function - simple

--- src/extract_data.py
+++ src/extract_data.py
@@ -9,19 +9,19 @@ async def get_player_info():
     session = aiohttp.ClientSession()
     fpl = FPL(session)
 
-    players = await fpl.get_players(return_json=True)
+    players = await fpl.get_players(return_json=True, include_summary=True)
 
     players_table = pd.DataFrame.from_dict(players)
     players_table.sort_values(by='goals_scored', inplace=True)
     return players_table
 
 
-def get_history(df:pd.DataFrame):
+def get_history(df:pd.DataFrame, history='history'):
     """
     Need to understand what each level refers to e.g. Current game, all games?
     """
     players_table_history = []
-    for id, name, player in zip(df.id, df.first_name, df.history):
+    for id, name, player in zip(df.id, df.first_name, df[history]):
         for i in range(len(player)):
             player[i]['id'] = id
             player[i]['name'] = name
@@ -52,6 +52,32 @@ Own goal = -2
 
 """
 
-def calculate_player_score():
+def calculate_player_score(df):
+    #'points_per_game'
+    goals_scored_points = {''}
+    df.loc[:, 'player_score'] = df.apply(lambda x: x.goals_scored )
+    'goals_score'
+    'assists'
 
     return True
+
+
+if __name__ == '__main__':
+
+    players_table = asyncio.run(get_player_info())
+    print(players_table.head())
+
+    players_history = get_history(players_table, 'history')
+    print(players_history.columns)
+    players_history_past = get_history(players_table, 'history_past')
+    print(players_history_past.columns)
+
+    players_table = players_history.merge(players_table, how='outer')
+    players_table = players_history_past.merge(players_table, how='outer')
+
+    print(players_table.head())
+    print([x for x in players_table.columns])
+
+    print(players_table.form.value_counts())
+
+    #:TODO Find out positions of players to produce score.
